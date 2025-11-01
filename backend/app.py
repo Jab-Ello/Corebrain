54de7390ac65a4158fd83825c86b33ec5323cb47
Jab-Ello <jabran.ellaoui@gmail.com>
2025-11-01 17:02:52 +0100

    - App.py update
    - CORS modifications

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ models.Base.metadata.create_all(bind=engine)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://109.129.246.230:3000"],
     allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
 
